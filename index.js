649b9a7a672a9116f02d0a6fe9087084b5d5e9dc
josevsd <88160424+josevsd@users.noreply.github.com>
2022-03-15 21:21:35 -0300

    commit init

---
+++ index.js
@@ -0,0 +1,8 @@
+function imprimir(){
+    var palavra = document.getElementById('palavra').value
+    // let transferir = document.getElementById('transferir')
+
+    for (let i = 0; i <10; i++){
+        console.log(palavra)
+    }
+}
